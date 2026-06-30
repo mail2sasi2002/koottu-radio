6b1b9f75c7bd56a448778c2fe15180f6904ed945
mail2sasi2002 <99566984+mail2sasi2002@users.noreply.github.com>
2026-06-29 21:37:18 -0500

    Update label-archive.js

--- label-archive.js
+++ label-archive.js
@@ -29,6 +29,12 @@
       // Nicely formatted title to show in the widget
       title: "08-03-2026 - திருஅண்ணாமலை"
     },
+     {
+      // EXACT Blogger label text (no spaces before/after last dash)
+      key: "24-05-2026-திருஅண்ணாமலை",
+      // Nicely formatted title to show in the widget
+      title: "24-05-2026 - திருஅண்ணாமலை"
+    },
     {
       // EXACT Blogger label text (no spaces before/after last dash)
       key: "02-11-2025-திருஅண்ணாமலை",
